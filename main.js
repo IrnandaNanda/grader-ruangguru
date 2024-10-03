236379feffb5742fb22d39882bbc4f6f0d99c766
Irnanda <139690628+IrnandaNanda@users.noreply.github.com>
2024-10-03 23:14:29 +0700

    js-exercise-conditional-holly-knight-v2

--- main.js
+++ main.js
@@ -1,40 +1,16 @@
-function rockPaperScissor(player1, player2) {
-
-    //Kondisi Draw
-    if (player1 === player2 ){
-      return 'Draw!';
-    }
-
-    //Kondisi Player 1
-    if (player1 === 'paper'){
-      if (player2 === 'rock'){
-        return 'Player 1 Won!';
-      } else {
-        return 'Player 2 Won!';
-      }
-    }
-
-    if (player1 === 'rock'){
-      if (player2 === 'scissor'){
-        return 'Player 1 Won!';
-      } else {
-        return 'Player 2 Won!';
-      }
-    }
-
-    if (player1 === 'scissor'){
-      if (player2 === 'paper'){
-        return 'Player 1 Won!';
-      } else {
-        return 'Player 2 Won!';
-      }
-    }
+function hollyKnight(name, stamina, undead) {
+  if (stamina > undead) {
+    return 'Holly Knight ' + name + ' memenangkan pertempuran dengan mudah!';
+  } else if (stamina < undead) {
+    return 'Holly knight ' + name + ' mengalahkan '+ stamina +' undead, namun sayang holly knight '+ name +' gugur di medan perang!'
+  } else {
+    return 'Beruntung Holly knight '+ name +' berhasil mengalahkan '+ undead +' undead!'
   }
-  
-  console.log(rockPaperScissor('scissor', 'paper')); // "Player 1 Won!"
-  console.log(rockPaperScissor('rock', 'paper')); // "Player 2 Won!"
-  console.log(rockPaperScissor('paper', 'paper')); // "Draw!"
-  console.log(rockPaperScissor('rock', 'rock')); // "Draw!"
-  
-  module.exports = rockPaperScissor;
-  
+}
+
+
+console.log(hollyKnight('Lancelot', 30, 20)); // Holly Knight Lancelot memenangkan pertempuran dengan mudah!
+console.log(hollyKnight('Gallahad', 10, 10)); // Beruntung Holly knight Gallahad berhasil mengalahkan 10 undead!
+console.log(hollyKnight('Tristan', 100, 110)); // Holly knight Tristan mengalahkan 100 undead, namun sayang holly knight Tristan gugur di medan perang!
+
+module.exports = hollyKnight;
